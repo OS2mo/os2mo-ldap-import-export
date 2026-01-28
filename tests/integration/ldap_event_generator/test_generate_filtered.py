9c2c7751af0c106976a080a17a51f6c5c6d28e17
Emil Madsen <githubpublicemail.goldmine822@passinbox.com>
2026-01-28 20:03:22 +0100

    test(ldap_event_generator): [#66796] prove that LDAPEventGenerator is unfiltered
    This commit adds a test to cement the current behavior of the
    LDAPEventGenerator with regards to its filtering of entities based on
    their object class, or more correctly its lack of such filtering
    alltogether.

---
+++ tests/integration/ldap_event_generator/test_generate_filtered.py
@@ -0,0 +1,45 @@
+# SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
+# SPDX-License-Identifier: MPL-2.0
+
+import pytest
+from httpx import AsyncClient
+
+from mo_ldap_import_export.ldap_event_generator import MICROSOFT_EPOCH
+from mo_ldap_import_export.types import LDAPUUID
+from mo_ldap_import_export.utils import combine_dn_strings
+from tests.integration.conftest import DNList2UUID
+
+
+@pytest.mark.integration_test
+@pytest.mark.envvar(
+    {
+        "LISTEN_TO_CHANGES_IN_LDAP": "True",
+        "LISTEN_TO_CHANGES_IN_MO": "False",
+    }
+)
+async def test_generate_filtered(
+    test_client: AsyncClient,
+    dnlist2uuid: DNList2UUID,
+    ldap_person_uuid: LDAPUUID,
+    ldap_org_unit_uuid: LDAPUUID,
+    ldap_org: list[str],
+) -> None:
+    ldap_org_uuid = await dnlist2uuid(ldap_org)
+
+    # Use the internal endpoint to fetch changes instead of waiting for the poller
+    # This proves that the poll logic itself is filtering correctly
+    since = MICROSOFT_EPOCH.isoformat()
+    search_base = combine_dn_strings(ldap_org)
+    response = await test_client.request(
+        "GET",
+        f"/ldap_event_generator/{since}",
+        content=search_base,
+        headers={"Content-Type": "text/plain"},
+    )
+    assert response.status_code == 200
+
+    # The ldap_person_uuid fixture ensures an inetOrgPerson exists.
+    # It also creates an organizationalUnit and an organization.
+    # We all of them to be present here.
+    uuids = {LDAPUUID(u) for u in response.json()}
+    assert uuids == {ldap_person_uuid, ldap_org_unit_uuid, ldap_org_uuid}
